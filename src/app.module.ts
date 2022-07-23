4e3832a0d58e979a8a99d050061187ff1aeb77ef
PNinadMS <ninad.parkar@techno-soft.in>
2022-07-23 17:28:22 +0530

    feat: mongo connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/demo'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
